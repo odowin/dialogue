3a895c3108f53562c620d2da107db89f613db21c
odowin <102875944+odowin@users.noreply.github.com>
2024-01-22 16:45:17 +0100

    Update timeline.php

--- timeline.php
+++ timeline.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=dialogue", 'root', '', [
+$pdo = new PDO("mysql:host=localhost;dbname=xxxxx", 'xxxxx', 'xxxxx', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 ]);
